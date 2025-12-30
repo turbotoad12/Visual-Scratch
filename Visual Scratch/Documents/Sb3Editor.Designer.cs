982fcf637c63df6131ffc40b3cd851a2938969d5
TurboToad12 <jones_zachary@outlook.com>
2025-12-30 17:49:49 -0600

    Add Sb3Editor Document.

---
+++ Visual Scratch/Documents/Sb3Editor.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Visual_Scratch
+{
+    partial class Sb3Editor
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.webView21.Location = new System.Drawing.Point(0, 0);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(721, 448);
+            this.webView21.TabIndex = 0;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // Sb3Editor
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.Controls.Add(this.webView21);
+            this.Name = "Sb3Editor";
+            this.Size = new System.Drawing.Size(721, 448);
+            this.Load += new System.EventHandler(this.Sb3Editor_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+    }
+}
---
+++ Visual Scratch/Documents/Sb3Editor.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.WinForms;
+
+namespace Visual_Scratch
+{
+    public partial class Sb3Editor : UserControl
+    {
+        public string Sb3FilePath { get; set; }
+        public Sb3Editor(string sb3FilePath)
+        {
+            InitializeComponent();
+            Sb3FilePath = sb3FilePath;
+        }
+        public static void LoadSb3IntoTurboWarp(WebView2 webView, string sb3Path)
+        {
+            if (!File.Exists(sb3Path))
+            {
+                throw new FileNotFoundException("SB3 file not found", sb3Path);
+            }
+            byte[] sb3Bytes = File.ReadAllBytes(sb3Path);
+            string base64 = Convert.ToBase64String(sb3Bytes);
+            string js = $@" (async () => {{ const base64 = '{base64}'; const binary = atob(base64); const array = new Uint8Array(binary.length); for (let i = 0; i < binary.length; i++) {{ array[i] = binary.charCodeAt(i); }} const blob = new Blob([array], {{ type: 'application/x.scratch.sb3' }}); const project = await blob.arrayBuffer(); window.vm.loadProject(project); }})(); ";
+            webView.CoreWebView2.ExecuteScriptAsync(js);
+        }
+
+        private void webView21_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private async void Sb3Editor_Load(object sender, EventArgs e)
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+            webView21.CoreWebView2.NavigationCompleted += (s, args) =>
+            {
+                LoadSb3IntoTurboWarp(webView21, Sb3FilePath);
+            };
+            webView21.CoreWebView2.Navigate("https://turbowarp.org/editor");
+        }
+    }
+}
